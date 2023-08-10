9bc4eb40289f1fdf21f0c2832340c263bc53f46c
Nebiyu <nebiyuhiyabu@gmail.com>
2023-08-10 14:40:44 +0000

    Country enumeration

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,5 @@
+-- Using ENUM to enumerate country values
+CREATE TABLE IF NOT EXISTS users(id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM("US", "CO", "TN") DEFAULT 'US' NOT NULL);
