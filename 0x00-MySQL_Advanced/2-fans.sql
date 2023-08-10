75c2df3504c1a7e732948e7d65d624c2fe29c445
Nebiyu <nebiyuhiyabu@gmail.com>
2023-08-10 15:59:50 +0000

    display name changed

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,2 @@
 -- getting the bands with the highest fans with their origin
-ALTER TABLE metal_bands CHANGE fans nb_fans INT(11);
-SELECT origin, nb_fans FROM metal_bands ORDER BY nb_fans DESC;
+SELECT origin, fans AS nb_fans FROM metal_bands ORDER BY nb_fans DESC;
