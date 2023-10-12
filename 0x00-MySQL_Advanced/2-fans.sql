d26f022eb032f092ea30dd33b1c9b26e3fbe1d2a
Nebiyu <nebiyuhiyabu@gmail.com>
2023-10-12 09:16:04 +0000

    2-fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,2 +1,2 @@
 -- getting the bands with the highest fans with their origin
-SELECT origin, fans AS nb_fans FROM metal_bands ORDER BY nb_fans DESC;
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
---
+++ 0x00-MySQL_Advanced/start.sh
@@ -0,0 +1,6 @@
+cat 0-uniq_users.sql | mysql -uroot -p holberton
+echo "SELECT * FROM tv_genres" | mysql -uroot -proot hbtn_0d_tvshows
+echo 'INSERT INTO users (email, name) VALUES ("bob@dylan.com", "Bob");' | mysql -uroot -proot holberton
+echo 'INSERT INTO users (email, name) VALUES ("sylvie@dylan.com", "Sylvie");' | mysql -uroot -proot holberton
+echo 'INSERT INTO users (email, name) VALUES ("bob@dylan.com", "Jean");' | mysql -uroot -proot holberton
+echo "SELECT * FROM users;" | mysql -uroot -proot holberton
